71b5de27941b93af224ac5aeff47929bbf36cf89
moonsu8 <50522031+moonsu8@users.noreply.github.com>
2021-01-08 13:00:04 +0900

    g
    g

---
+++ js/youtube_hanzi.js
@@ -0,0 +1,10 @@
+//2020-03-04 타래료 1화
+
+var you_data = [
+				["他","tā","그, 그녀","1我搬走了 房东就会找别人住进来", "1wǒ bān zǒu le fángdōng jiù huì zhǎo biérén zhù jìnlai", "1내가 이사가면 집주인이 다른 세입자를 찾을거야"],
+				["薄","zhǎng","시간","2我搬走了 房东就会找别人住进来", "2wǒ bān zǒu le fángdōng jiù huì zhǎo biérén zhù jìnlai", "2내가 이사가면 집주인이 다른 세입자를 찾을거야"],
+				["越","děng","유머","3我搬走了 房东就会找别人住进来", "3wǒ bān zǒu le fángdōng jiù huì zhǎo biérén zhù jìnlai", "3내가 이사가면 집주인이 다른 세입자를 찾을거야"],
+				["大","tíng","선생님","4我搬走了 房东就会找别人住进来", "4wǒ bān zǒu le fángdōng jiù huì zhǎo biérén zhù jìnlai", "4내가 이사가면 집주인이 다른 세입자를 찾을거야"],
+				["那","jiù","빌려주시면","5我搬走了 房东就会找别人住进来", "5wǒ bān zǒu le fángdōng jiù huì zhǎo biérén zhù jìnlai", "5내가 이사가면 집주인이 다른 세입자를 찾을거야"],
+				["大","duō","감각이","6我搬走了 房东就会找别人住进来", "6wǒ bān zǒu le fángdōng jiù huì zhǎo biérén zhù jìnlai", "6내가 이사가면 집주인이 다른 세입자를 찾을거야"]
+			];
