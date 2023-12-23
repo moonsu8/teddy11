8f6045b98ac8f0522739dd6157b0f569e5fe74f0
moonsu8 <50522031+moonsu8@users.noreply.github.com>
2023-12-23 23:38:06 +0900

    dfd
    fdfdf

---
+++ js/movieList.js
@@ -0,0 +1,58 @@
+
+//<!-- 상단 전체 영화 콤보박스 DATA -->
+// 유튜브 영화 LIST 정보 
+
+
+var movieList = 
+
+{
+	"talaile" : [
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 1,
+            "url" : "SjCcXmqO8GE"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 2,
+            "url" : "ha1TJPLuBdY"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 3,
+            "url" : "OOat7kv36Pg"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 4,
+            "url" : "rk5j5gQ8UNU"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 5,
+            "url" : "5cM5qzZRkAc"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 6,
+            "url" : "hLiDPNs0ADk"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 7,
+            "url" : "YdUBMaSTGQA"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 9,
+            "url" : "OuxmeMc_ZJM"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 10,
+            "url" : "aWcjtjXmA5w"
+        } 
+    ]
+
+
+}
